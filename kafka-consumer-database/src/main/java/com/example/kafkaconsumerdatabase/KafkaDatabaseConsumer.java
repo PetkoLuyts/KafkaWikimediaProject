9af3273b7e3d7c34f5e00519ddd28f8344dda810
petko.lyutskanov <simona2000petko@abv.bg>
2022-09-30 16:11:39 +0300

    Save wikimedia data into MySql database

--- kafka-consumer-database/src/main/java/com/example/kafkaconsumerdatabase/KafkaDatabaseConsumer.java
+++ kafka-consumer-database/src/main/java/com/example/kafkaconsumerdatabase/KafkaDatabaseConsumer.java
@@ -1,5 +1,7 @@
 package com.example.kafkaconsumerdatabase;
 
+import com.example.kafkaconsumerdatabase.entity.WikimediaData;
+import com.example.kafkaconsumerdatabase.repository.WikimediaDataRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -9,8 +11,19 @@ import org.springframework.stereotype.Service;
 public class KafkaDatabaseConsumer {
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
 
+    private WikimediaDataRepository dataRepository;
+
+    public KafkaDatabaseConsumer(WikimediaDataRepository dataRepository) {
+        this.dataRepository = dataRepository;
+    }
+
     @KafkaListener(topics = "wikimedia-recent-change", groupId = "myGroup")
     public void consume(String eventMessage) {
         LOGGER.info(String.format("Event message received -> %s", eventMessage));
+
+        WikimediaData wikimediaData = new WikimediaData();
+        wikimediaData.setWikiEventData(eventMessage);
+
+        dataRepository.save(wikimediaData);
     }
 }
